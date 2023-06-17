273692b2ce5d12bc559e4e75c35c1cc7f4564b3b
isszero06 <76986925+isszero06@users.noreply.github.com>
2023-06-17 18:55:41 +0300

    Add files via upload

---
+++ zero_journal_entries_print/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- encoding: UTF-8 -*-
+{
+    'name': 'Print Journal Entries (PDF)',
+    'version': '5.1.0',
+        "author": 'Zero Systems',
+        "company": 'Zero for Information Systems',
+         "website": "https://erpzero.com",
+        "email": "sales@erpzero.com",
+    'category': 'Accounting',
+    'sequence': 1,
+    'summary': 'Print Journal Entries PDF',
+    'description': """
+    this module use for print journal Entries in PDF report"
+    """,
+    'depends': ['account'],
+    'data': [
+        'report/report.xml'
+    ],
+    "price": 15.00,
+    "currency": 'EUR',
+    'license': 'OPL-1',
+    'installable': True,
+    'auto_install': False,
+    "application": True,
+    'pre_init_check_vers': 'pre_init_check_vers',
+    'images': ['static/description/logo.PNG'],
+}
